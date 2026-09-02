aa2728996cf9ff40dcd82f9bd0d8c9b2f2059777
veera <veerababu.narni@gmail.com>
2026-09-02 08:45:50 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+# Create a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Loop through the list to calculate squares
+squares = []
+for num in numbers:
+    squares.append(num ** 2)
+
+print("Original list:", numbers)
+print("Squared list:", squares)
