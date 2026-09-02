a4d24d4b2414de0eca4a9aaa698f774a1ebaa6f7
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-09-02 08:46:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ numbers = [1, 2, 3, 4, 5]
 # Loop through the list to calculate squares
 squares = []
 for num in numbers:
-    squares.append(num ** 2)
+    squares.append(num ** 3)
 
 print("Original list:", numbers)
 print("Squared list:", squares)
